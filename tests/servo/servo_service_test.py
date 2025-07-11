709047c83ecdd535f4be4b581c2c70646df09cbb
Brandon Tran <brandont3927@gmail.com>
2025-07-11 13:09:32 -0700

    temporarily remove printouts in servo test

--- tests/servo/servo_service_test.py
+++ tests/servo/servo_service_test.py
@@ -4,11 +4,11 @@ from auv.motion.robot_control import RobotControl
 def test_servo(service_name):
     rc = RobotControl(enable_dvl=False)
     try:
-        print(f"Testing servo: {service_name}")
+        # print(f"Testing servo: {service_name}")
         result = rc.move_servo(service_name)
         print(f"Result from {service_name}: {result}")
     except Exception as e:
-        print(f"Error testing {service_name}: {e}")
+        # print(f"Error testing {service_name}: {e}")
         import traceback
         traceback.print_exc()
 
