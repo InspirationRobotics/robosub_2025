d14a438a539ae5441647b2c655a189ce269f23a2
Parsa Sedighi <parsasedighi2004@icloud.com>
2025-07-20 18:34:24 -0700

    new test for modem_api

---
+++ tests/modem/modem_api_test.py
@@ -0,0 +1,25 @@
+# IMPORT modem class form modem_api.py
+from auv.device.modems.modems_api import Modem
+import time
+import rospy
+
+# ROS Publisher to modem_send 
+
+# Publish a string using the publish method
+
+# Initiliaze a modem class
+modem = Modem()
+
+rospy.init_node("modem_sender", anonymous=True)
+pub = rospy.Publisher("/auv/devices/modem_send", String, queue_size=10)
+
+if not rospy.is_shutdown():
+        # Format a message: "DEST-MSG-ACK-PRIORITY"
+        msg_str = "020-ROLL-1-0"
+
+        rospy.loginfo(f"Publishing: {msg_str}")
+        pub.publish(String(data=msg_str))
+
+        rate.sleep()
+
+# ROSPY Init Node
