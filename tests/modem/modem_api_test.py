2a418ba1ec9867b1b51d5f2d5f53a4e1c5e38781
Parsa Sedighi <parsasedighi2004@icloud.com>
2025-07-20 19:04:51 -0700

    Update modem_api_test

--- tests/modem/modem_api_test.py
+++ tests/modem/modem_api_test.py
@@ -2,7 +2,7 @@
 from auv.device.modems.modems_api import Modem
 import time
 import rospy
-
+from standard_msgs.msg import String
 # ROS Publisher to modem_send 
 
 # Publish a string using the publish method
@@ -13,13 +13,17 @@ modem = Modem()
 rospy.init_node("modem_sender", anonymous=True)
 pub = rospy.Publisher("/auv/devices/modem_send", String, queue_size=10)
 
+rate = rospy.Rate(1)  # 1 Hz
+
 if not rospy.is_shutdown():
-        # Format a message: "DEST-MSG-ACK-PRIORITY"
-        msg_str = "020-ROLL-1-0"
+    # Format a message: "DEST-MSG-ACK-PRIORITY"
+    msg_str = "020-ROLL-1-0"
+    rospy.loginfo(f"Publishing: {msg_str}")
+    pub.publish(String(data=msg_str))
+    rate.sleep()
 
-        rospy.loginfo(f"Publishing: {msg_str}")
-        pub.publish(String(data=msg_str))
 
-        rate.sleep()
+
+
 
 # ROSPY Init Node
