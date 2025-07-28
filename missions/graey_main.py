ef4884b96cb725d699c3c6f3faf559ce54c5c842
avikaprasad22 <avikapd22@gmail.com>
2025-07-28 14:26:30 -0700

    forward changed to 13 secs

--- missions/graey_main.py
+++ missions/graey_main.py
@@ -36,8 +36,8 @@ try:
     # going through the Gat
     curr_time = time.time()
 
-    rospy.loginfo("Moving forward for 10 seconds")
-    while time.time() - curr_time < 10:
+    rospy.loginfo("Moving forward for 13 seconds")
+    while time.time() - curr_time < 13:
         rc.movement(forward=2)
 
     rc.movement() # stop moving forward
