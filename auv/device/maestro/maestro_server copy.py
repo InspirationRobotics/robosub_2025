2c0bb1ec509da67e116abe0a9b99278407d20569
EMC641 <108538423+EMC641@users.noreply.github.com>
2025-07-30 14:29:24 -0700

    update

--- auv/device/maestro/maestro_server copy.py
+++ auv/device/maestro/maestro_server copy.py
@@ -27,6 +27,9 @@ class MaestroServer:
         self.has_dropped1_marker = False
         self.has_dropped2_marker = False
 
+        self.has_picked_item = False
+        self.has_released_item = False
+
         self.dropperService = rospy.Service('/auv/device/dropper', Trigger, self.dropperCallback)
         self.gripperService = rospy.Service('/auv/device/gripper', Trigger, self.gripperCallback)
         self.torpedoService = rospy.Service('/auv/device/torpedo', Trigger, self.torpedoCallback)
@@ -39,41 +42,6 @@ class MaestroServer:
         rospy.loginfo("Ready to take servo requests.")
         rospy.spin()
         
-    def dropperCallback(self, request):
-        rospy.loginfo("dropping a marker")
-       
-        if not self.has_dropped1_marker:
-            self.maestro.set_pwm(*self.dropper_state["dropping_first"])
-            self.has_dropped1_marker = True
-
-        elif not self.has_dropped2_marker:
-            self.maestro.set_pwm(*self.dropper_state["dropping_second"])
-            self.has_dropped2_marker = True
-
-        else:
-            # reset for the next cycle
-            self.maestro.set_pwm(*self.dropper_state["beginning_position"])
-            self.has_dropped1_marker = False
-            self.has_dropped2_marker = False
-
-        """self.maestro.set_pwm(0,1765)  # Move servo on channel 0
-        time.sleep(1.0)
-        self.maestro.set_pwm(0,1136)  # Move servo on channel 0
-        print("marker 1 dropped")
-        time.sleep(2.0) # adjust this number for the amount of time
-        self.maestro.set_pwm(0,750) # Move servo on channel 0
-        print("marker 2 dropped")
-        time.sleep(1.0)
-        self.maestro.set_pwm(0,1765)  # Move servo on channel 0
-        time.sleep(1)
-        """
-
-        return TriggerResponse(
-            success=True,
-            message="The markers are dropped!"
-        )
-
-
     def gripperCallback(self, request):
         rospy.loginfo("picking up item")
        
@@ -96,6 +64,36 @@ class MaestroServer:
             success=True,
             message="code complete!"
         )
+
+
+    def gripperCallback(self, request):
+        rospy.loginfo("Gripper has been triggered")
+
+        def gripper_state(state: str):
+            """Helper function for gripper state
+            Args:
+                - state: State (opening, static, closing)"""
+            if state not in ["opening", "static", "closing"]:
+                return ValueError("Incorrect state")
+            start_time = time.time()
+            while time.time() - start_time < 0.5:
+                self.maestro.set_pwm(*self.gripper_state[state])
+                time.sleep(0.05)
+
+        # Open gripper
+        gripper_state("opening")
+        
+        # Wait for 0.5 secs
+        gripper_state("static")
+
+        # Close gripper
+        gripper_state("closing")
+
+        return TriggerResponse(
+            success=True,
+            message="Triggered successfully!"
+        )
+
 
     def torpedoCallback(self, request):
         rospy.loginfo("launching torpedo")
