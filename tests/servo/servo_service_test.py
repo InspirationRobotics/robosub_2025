7ee0fbf66e560a0f01aefd83abcd94e5015021ca
EMC641 <108538423+EMC641@users.noreply.github.com>
2025-07-30 13:29:15 -0700

    update

--- tests/servo/servo_service_test.py
+++ tests/servo/servo_service_test.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
         rospy.sleep(2)
         test_servo("/auv/device/gripper")
         rospy.sleep(2)
-        test_servo("/auv/device/dropper")
+        """test_servo("/auv/device/dropper")
         rospy.sleep(2)
         test_servo('/auv/device/dropper')
         rospy.sleep(2)
@@ -41,7 +41,7 @@ if __name__ == "__main__":
         test_servo("/auv/device/torpedo")
         rospy.sleep(2)
         test_servo("/auv/device/torpedo")
-        rospy.sleep(2)
+        rospy.sleep(2)"""
         # Keep node alive until shutdown
         while not rospy.is_shutdown():
             rospy.sleep(0.1)
