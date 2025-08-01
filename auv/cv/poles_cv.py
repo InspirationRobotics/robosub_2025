ba83bdb5d1bb8c6667a22d92ca024aab2fd85ce9
avikaprasad22 <avikapd22@gmail.com>
2025-08-01 14:01:25 -0700

    got rid of speed decrease

--- auv/cv/poles_cv.py
+++ auv/cv/poles_cv.py
@@ -86,7 +86,6 @@ class CV:
             print(f"[INFO] Search time began: ({time.time() - self.start_time:.2f}s)")
 
             if self.search_start_time is not None and time.time() - self.search_start_time < 3.0:
-                forward = 0.9
                 self.end = True
                 print("[INFO] Completed slalom through poles → ending")
             else:
