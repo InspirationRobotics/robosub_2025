a6c3595eda8df70ebf89096392c30f86d0930e98
Chase <chace070528@gmail.com>
2025-08-08 11:09:15 -0700

    New search logic for bin approach

--- auv/cv/bin_approach_cv.py
+++ auv/cv/bin_approach_cv.py
@@ -39,20 +39,20 @@ class CV:
         self.prev_detected = False
         self.state = "search"
 
+        # Leonard: NOTE 
         # Most var names (except the last three) don't immediately give away their function, although when I looked
         # at the code it made sense. Calling a switch back from approach to search an "adjust" is a little bit quirky,
         # there may be a better name for it. A big thing in industry is not just functional, but also readable code. 
-        # Code isn't any good if the next person coming along has no idea what's happening.
-        self.swtich_back_time = None
-        self.search_counter = 0
-        self.search_stage_one_timestamp = None # store time
-        self.search_stage_two_timestamp = None # store time
+        # Code isn't any good if the next person coming along has no idea what's happening.  
+        
+        # Chase: Solved, change var name to switch_back_time
+        self.switch_back_time = None
         self.stage_two_end = False
         self.switch_count = 0
         self.end = False
         self.prev_offset = None
         self.prev_time = time.time()
-        
+        self.start_time = self.prev_time
         print("[INFO] Bin Approach CV Initialization")
     
     def smart_approach(self, offset: int) -> Tuple[float, float]:
@@ -83,6 +83,7 @@ class CV:
         return forward, yaw
 
     def process_detection_offset(self, detection):
+        """A function to calculate offset if there is detection, set state to approach"""
         self.target_x = (detection.xmin + detection.xmax) / 2
         self.target_y = (detection.ymin + detection.ymax) / 2
         self.curr_offset = self.target_x - self.framecenter_x # These var names could use some work. Both self.target_x and self.framecenter_x are technically midpoints - the former of the detection bounding box, the latter of the frame
@@ -93,7 +94,6 @@ class CV:
 
     
 
-    # Add type hints to this function
     def run(self, frame, target, detections):
         """
         Run the CV script.
@@ -121,8 +121,6 @@ class CV:
         # Configure search state if there aren't detections
         if detections is None:
             detections = []
-        if len(detections) == 0 and self.prev_detected == False:
-            self.state = "search"
         
         # Utilize bin detections with at least 55% confidence
         detected_list = []
@@ -133,7 +131,6 @@ class CV:
                 if det.confidence > detection_confidence:
                     detected_list.append(det)
 
-        # select the highest confidence Bin deteciton if multiple
         self.curr_offset = None
         if len(detected_list)==0:
             self.curr_offset = None
@@ -141,43 +138,13 @@ class CV:
             self.prev_time = time.time()
             detection = detected_list[0]
             self.process_detection_offset(detection=detection)
-
-        else:  # when there are more than one Bin detection
-            # Select the detection with the highest confidence
+        else: 
+            # select the highest confidence Bin deteciton if multiple
             self.prev_time = time.time()
             detection = max(detected_list, key=lambda det: det.confidence)
             detection_confidence = detection.confidence
             print(f"[DEBUG] Multiple Bins detected. Using highest confidence detection: {detection_confidence}")
             self.process_detection_offset(detection=detection)
-
-
-        if self.state == "search":
-            # For less than two searches, yaw left at 20% for 5 seconds then yaw right at 20% for 5 seconds
-            # This program will search left for a certain number of degrees then search right
-            if self.search_counter<=2:
-                if self.search_stage_one_timestamp is None:
-                    print("[DEBUG] Searching in stage 1")
-                    self.search_stage_one_timestamp = time.time()
-                if time.time()-self.search_stage_one_timestamp > 5 * (self.search_counter + 1):
-                    print(f"[DEBUG] Searching in stage one, counter is {self.search_counter}")
-                    self.search_counter += 1
-                    self.search_stage_one_timestamp = time.time()
-                if self.search_counter%2==1:
-                    yaw = 0.6
-                else:
-                    yaw = -0.6
-            else:
-            # Yaw in the direction where the bin was previously seen, or right if not seen
-                if self.search_stage_two_timestamp is None:
-                    print(f"[DEBUG] Searching in stage two")
-                    self.search_stage_two_timestamp = time.time()
-                
-                if self.prev_offset is None:
-                    yaw = 0.6
-                elif self.prev_offset > 0 :
-                    yaw = 0.6
-                elif self.prev_offset < 0:
-                    yaw = -0.6
 
         if self.state == "approach":
         # If a detection exists, end search stage two and approach
@@ -190,36 +157,32 @@ class CV:
             
         # Check Ending
 
-        # I don't see any place in the script where self.prev_detected is set to None. A quick test 
-        # reveals False is not None. You need a better condition since at the present state, the script will never end.
-
-        # Also, what if we've made it over the bins in less than 30 seconds after search_stage_two begins? Substitude self.prev_detected with self.prev_offset
-        if self.state=="search" and self.prev_offset is None and self.search_stage_two_timestamp is not None and time.time()-self.search_stage_two_timestamp > 30:
-           # when we had went through stage one and time out for 30 seconds
-           print(f"[DEBUG] time out in searching")
-           self.end = True
+        # Ending 1: Total Misison Time Out
+        if time.time() - self.start_time > 120: # Arbitary set to 120 s for now, need to see what's the estimated mission time including searching
+            self.end = True
 
         # End the script if we've lost the bins and then was searching for 15 seconds. This should result in
         # yawing for 15 seconds for a full 360 degrees. In theory, it should work if we're on top of the bins
         # (meaning that we won't find it). In practice, the model needs to be robust enough to prevent false positives.
         # Also, what if we are close to (but not on top of) the bins so that it's out of the FOV? I'll need to inspect
         # the drop script to see if you account for that.
-
-        if self.state=="search" and self.prev_detected: # you are in adjust search mode when you had detection but in search mode again
-            if time.time() - self.swtich_back_time > 15:
+        
+        # Ending 2: Lost target durnig approaching and can't pick up detection again (Only when we have the ability to search again after lost target during approaching)
+        if self.state=="search" and self.prev_detected:
+            if time.time() - self.switch_back_time > 15:
                 self.end = True
 
-        # If we lost the bins for two seconds in approach mode, change to search mode up to two times per
-        # mission attempt and start a timer for 15 seconds.
+        # Ending 3: Lost target for 6.5 s continuously, either end the mission directly or switch back to search state
         if self.state=="approach" and (self.curr_offset is None) and self.prev_detected == True:
             lost_detection_time = time.time() - self.prev_time
             print(f"Lost detection for {lost_detection_time} s during approaching")
             if  lost_detection_time> 6.5:
+                """Uncomment the following code if you want to have the ability to search again after lost target during approaching"""
                 # if self.switch_count <2:  # switch back to search again
                 #     print(f"[DEBUG] switch back to search state")
                 #     self.state = "search"
                 #     self.switch_count += 1
-                #     self.swtich_back_time = time.time()
+                #     self.switch_back_time = time.time()
 
                 # else:
                 #     print(f"[DEBUG] Ending with prev detected: {self.prev_detected}")
@@ -228,4 +191,4 @@ class CV:
                 self.end = True
         
         # Continuously return motion commands, the state of the mission, and the visualized frame.
-        return {"lateral": lateral, "forward": forward, "yaw": yaw, "vertical" : vertical, "end": self.end}, frame
+        return {"state": self.state, "prev_offset": self.prev_offset,"lateral": lateral, "forward": forward, "yaw": yaw, "vertical" : vertical, "end": self.end}, frame
