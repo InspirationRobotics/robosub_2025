31bc36e7c21457bd61b0daed62b43c6f5372e8c7
Leonard Wright <leonardalwright@gmail.com>
2025-08-12 07:52:28 -0700

    Push changes to auto script (fix mavros?)

--- scripts/mavros.sh
+++ scripts/mavros.sh
@@ -2,9 +2,14 @@
 
 PRODUCT=$(sudo lshw -json | jq '.product') || PRODUCT=$(sudo lshw -json | jq '.[].product')
 
+# cd into absolute path of robosub repo
+AUV_PATH=$(dirname $(dirname $(realpath $0)))
+echo $AUV_PATH
+cd $AUV_PATH
+
 if [[ $PRODUCT == *"Xavier"* ]]; then
   echo "Detected $PRODUCT setting to Xavier init"
-  POLULU=$(/usr/bin/python3 /home/inspiration/auv/auv/utils/deviceHelper.py polulu)
+  POLULU=$(/usr/bin/python3 -m auv.utils.devieHelper polulu)
   echo "Found Polulu Servo driver at $POLULU"
   screen -dmS polulu bash -c "bash /home/inspiration/auv/maestro-linux/clearPoluluErrors.sh $POLULU"
   DISTRO="noetic"
@@ -14,17 +19,13 @@ if [[ $PRODUCT == *"Nano"* ]]; then
   DISTRO="melodic"
 fi
 
-OUTPUT=$(/usr/bin/python3 /home/inspiration/auv/auv/utils/deviceHelper.py)
+OUTPUT=$(/usr/bin/python3 -m auv.utils.deviceHelper)
 
 echo "Found pixhawk on "${OUTPUT}
 screen -dmS roscore bash -c "source /opt/ros/$DISTRO/setup.bash ; roscore"
 
 # mavros crashes, need to fix
 screen -dmS mavros bash -c "source /opt/ros/$DISTRO/setup.bash ; sleep 5 ; roslaunch --wait mavros px4.launch fcu_url:=$OUTPUT"
-
-AUV_PATH=$(dirname $(dirname $(realpath $0)))
-echo $AUV_PATH
-cd $AUV_PATH
 
 screen -dmS cams bash -c "sleep 10 ; /usr/bin/python3 -m auv.device.camsVersatile"
 screen -dmS imu bash -c "/usr/bin/python3 -m auv.device.imu.vn100_serial"
