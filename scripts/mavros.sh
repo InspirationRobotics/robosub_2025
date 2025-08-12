b7c2482af64a991d67fcb684131eac7de8523ac3
Leonard Wright <leonardalwright@gmail.com>
2025-08-12 09:08:09 -0700

    Add modprobe

--- scripts/mavros.sh
+++ scripts/mavros.sh
@@ -25,8 +25,9 @@ echo "Found pixhawk on "${PORT}
 screen -dmS roscore bash -c "source /opt/ros/$DISTRO/setup.bash ; roscore"
 screen -dmS mavros bash -c "source /opt/ros/$DISTRO/setup.bash ; sleep 5 ; roslaunch --wait mavros px4.launch fcu_url:=$PORT"
 
-# Wait for mavros to start
+# Wait for mavros to start then set modprobe for cams
 sleep 15
+sudo modprobe -r v4l2loopback
 
 # Requires SSHPASS var to be set in ~/.bashrc
 screen -dmS cams bash -c "sleep 10 ; sshpass -e /usr/bin/python3 -m auv.device.camsVersatile"
